845de285d4f41f5bd0e78bf7c749b027207398f1
Lkiku <447140443@qq.com>
2021-04-01 15:22:50 +0800

    Create my_hw03.py

---
+++ 李宏毅ML2021/hw3/my_hw03.py
@@ -0,0 +1,359 @@
+# -*- coding: utf-8 -*-
+
+"""
+# **Homework 3 - Convolutional Neural Network**
+
+This is the example code of homework 3 of the machine learning course by Prof. Hung-yi Lee.
+
+In this homework, you are required to build a convolutional neural network for image classification, possibly with some advanced training tips.
+
+
+There are three levels here:
+
+**Easy**: Build a simple convolutional neural network as the baseline. (2 pts)
+
+**Medium**: Design a better architecture or adopt different data augmentations to improve the performance. (2 pts)
+
+**Hard**: Utilize provided unlabeled data to obtain better results. (2 pts)
+
+## **About the Dataset**
+
+The dataset used here is food-11, a collection of food images in 11 classes.
+
+For the requirement in the homework, TAs slightly modified the data.
+Please DO NOT access the original fully-labeled training data or testing labels.
+
+Also, the modified dataset is for this course only, and any further distribution or commercial use is forbidden.
+
+I just change the .ipynb to .py file.
+"""
+
+# Import necessary packages.
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+from PIL import Image
+# "ConcatDataset" and "Subset" are possibly useful when doing semi-supervised learning.
+from torch.utils.data import ConcatDataset, DataLoader, Subset
+from torchvision.datasets import DatasetFolder
+
+# This is for the progress bar.
+from tqdm.auto import tqdm
+
+# Specify the graphics card
+torch.cuda.set_device(7)
+
+# hyper-parameters 
+config = {
+    'learning_rate': 0.0001,       # learning rate
+    'batch_size': 64,
+    'num_epoch': 80,
+    'val_ratio': 0.2,       
+    'model_path': './train_model/model.ckpt',
+    'weight_decay': 1e-5
+}
+
+class Classifier(nn.Module):
+    def __init__(self):
+        super(Classifier, self).__init__()
+        # The arguments for commonly used modules:
+        # torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        # torch.nn.MaxPool2d(kernel_size, stride, padding)
+
+        # input image size: [3, 128, 128]
+        self.cnn_layers = nn.Sequential(
+            nn.Conv2d(3, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),
+
+            nn.Conv2d(64, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),
+
+            nn.Conv2d(128, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(4, 4, 0),
+        )
+        self.fc_layers = nn.Sequential(
+            nn.Linear(256 * 8 * 8, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 11)
+        )
+
+    def forward(self, x):
+        # input (x): [batch_size, 3, 128, 128]
+        # output: [batch_size, 11]
+
+        # Extract features by convolutional layers.
+        x = self.cnn_layers(x)
+
+        # The extracted feature map must be flatten before going to fully-connected layers.
+        x = x.flatten(1)
+
+        # The features are transformed by fully-connected layers to obtain the final logits.
+        x = self.fc_layers(x)
+        return x
+
+def prep_dataloader(batch_size, data_root='./food-11/'):
+
+    # It is important to do data augmentation in training.
+    # However, not every augmentation is useful.
+    # Please think about what kind of augmentation is helpful for food recognition.
+    train_tfm = transforms.Compose([
+        # Resize the image into a fixed shape (height = width = 128)
+        transforms.Resize((128, 128)),
+        transforms.RandomHorizontalFlip(p=0.5),   #随机水平翻转 选择一个概率
+        transforms.RandomRotation(45),            #随机旋转，-45到45度之间随机选
+        transforms.ColorJitter(brightness=0.2, contrast=0.1, saturation=0.1, hue=0.1),#参数1为亮度，参数2为对比度，参数3为饱和度，参数4为色相
+        transforms.RandomGrayscale(p=0.025),      #概率转换成灰度率，3通道就是R=G=B
+        # You may add some transforms here.
+        # ToTensor() should be the last one of the transforms.
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])#均值，标准差
+    ])
+
+    # We don't need augmentations in testing and validation.
+    # All we need here is to resize the PIL image and transform it into Tensor.
+    test_tfm = transforms.Compose([
+        transforms.Resize((128, 128)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])#均值，标准差
+    ])
+
+    # Construct datasets.
+    # The argument "loader" tells how torchvision reads the data.
+    train_set_path = data_root + "training/labeled"
+    valid_set_path = data_root + "validation"
+    unlabeled_set_path = data_root + "training/unlabeled"
+    test_set_path = data_root + "testing"
+    train_set = DatasetFolder(train_set_path, loader=lambda x: Image.open(x), extensions="jpg", transform=train_tfm)
+    valid_set = DatasetFolder(valid_set_path, loader=lambda x: Image.open(x), extensions="jpg", transform=test_tfm)
+    unlabeled_set = DatasetFolder(unlabeled_set_path, loader=lambda x: Image.open(x), extensions="jpg", transform=train_tfm)
+    test_set = DatasetFolder(test_set_path, loader=lambda x: Image.open(x), extensions="jpg", transform=test_tfm)
+
+    # Construct data loaders.
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=0, pin_memory=True)
+    valid_loader = DataLoader(valid_set, batch_size=batch_size, shuffle=True, num_workers=0, pin_memory=True)
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
+
+    return train_loader, valid_loader, test_loader, train_set, valid_set, unlabeled_set, test_set
+
+#check device
+def get_device():
+  return 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# The function "get_pseudo_labels" is used for semi-supervised learning.
+def get_pseudo_labels(dataset, dataloader, model, threshold=0.65):
+    # This functions generates pseudo-labels of a dataset using given model.
+    # It returns an instance of DatasetFolder containing images whose prediction confidences exceed a given threshold.
+    # You are NOT allowed to use any models trained on external data for pseudo-labeling.
+    device = get_device()
+
+    # Make sure the model is in eval mode.
+    model.eval()
+    # Define softmax function.
+    softmax = nn.Softmax(dim=-1)
+
+    # Iterate over the dataset by batches.
+    for batch in tqdm(dataloader):
+        img, _ = batch
+
+        # Forward the data
+        # Using torch.no_grad() accelerates the forward process.
+        with torch.no_grad():
+            logits = model(img.to(device))
+
+        # Obtain the probability distributions by applying softmax on logits.
+        probs = softmax(logits)
+
+        # ---------- TODO ----------
+        # Filter the data and construct a new dataset.
+
+    # # Turn off the eval mode.
+    model.train()
+    return dataset
+
+def train_data(train_loader, valid_loader, train_set, unlabeled_set, model_path, device, learning_rate, weight_decay, num_epoch, batch_size, do_semi = False):
+
+    # "cuda" only when GPUs are available.
+    device = device
+
+    # Initialize a model, and put it on the device specified.
+    model = Classifier().to(device)
+    model.device = device
+
+    # For the classification task, we use cross-entropy as the measurement of performance.
+    criterion = nn.CrossEntropyLoss()
+
+    # Initialize optimizer, you may fine-tune some hyperparameters such as learning rate on your own.
+    optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate, weight_decay=weight_decay)
+
+    # The number of training epochs.
+    n_epochs = num_epoch
+
+    # Whether to do semi-supervised learning.
+    do_semi = do_semi
+
+    best_acc = 0.0
+    for epoch in range(n_epochs):
+        # ---------- TODO ----------
+        # In each epoch, relabel the unlabeled dataset for semi-supervised learning.
+        # Then you can combine the labeled dataset and pseudo-labeled dataset for the training.
+        if do_semi:
+            # Obtain pseudo-labels for unlabeled data using trained model.
+            pseudo_set = get_pseudo_labels(unlabeled_set, model)
+
+            # Construct a new dataset and a data loader for training.
+            # This is used in semi-supervised learning only.
+            concat_dataset = ConcatDataset([train_set, pseudo_set])
+            train_loader = DataLoader(concat_dataset, batch_size=batch_size, shuffle=True, num_workers=0, pin_memory=True)
+        
+        # ---------- Training ----------
+        # Make sure the model is in train mode before training.
+        model.train()    
+
+        # These are used to record information in training.
+        train_loss = []
+        train_accs = []
+
+        # Iterate the training set by batches.
+        for batch in tqdm(train_loader):
+            # A batch consists of image data and corresponding labels.
+            imgs, labels = batch
+
+            # Forward the data. (Make sure data and model are on the same device.)
+            logits = model(imgs.to(device))
+
+            # Calculate the cross-entropy loss.
+            # We don't need to apply softmax before computing cross-entropy as it is done automatically.
+            loss = criterion(logits, labels.to(device))
+
+            # Gradients stored in the parameters in the previous step should be cleared out first.
+            optimizer.zero_grad()
+
+            # Compute the gradients for parameters.
+            loss.backward()
+
+            # Clip the gradient norms for stable training.
+            grad_norm = nn.utils.clip_grad_norm_(model.parameters(), max_norm=10)
+
+            # Update the parameters with computed gradients.
+            optimizer.step()
+
+            # Compute the accuracy for current batch.
+            acc = (logits.argmax(dim=-1) == labels.to(device)).float().mean()
+            
+            # Record the loss and accuracy.
+            train_loss.append(loss.item())
+            train_accs.append(acc)
+        
+        # The average loss and accuracy of the training set is the average of the recorded values.
+        train_loss = sum(train_loss) / len(train_loss)
+        train_acc = sum(train_accs) / len(train_accs)
+
+        # Print the information.
+        print(f"[ Train | {epoch + 1:03d}/{n_epochs:03d} ] loss = {train_loss:.5f}, acc = {train_acc:.5f}")
+
+        # ---------- Validation ----------
+        # Make sure the model is in eval mode so that some modules like dropout are disabled and work normally.
+        model.eval()
+
+        # These are used to record information in validation.
+        valid_loss = []
+        valid_accs = []
+
+        # Iterate the validation set by batches.
+        for batch in tqdm(valid_loader):
+            imgs, labels = batch
+            # We don't need gradient in validation.
+            # Using torch.no_grad() accelerates the forward process.
+            with torch.no_grad():
+                logits = model(imgs.to(device))
+            
+            # We can still compute the loss (but not the gradient).
+            loss = criterion(logits, labels.to(device))
+
+            # Compute the accuracy for current batch.
+            acc = (logits.argmax(dim=-1) == labels.to(device)).float().mean()
+
+            # Record the loss and accuracy.
+            valid_loss.append(loss.item())
+            valid_accs.append(acc)
+
+        # The average loss and accuracy for entire validation set is the average of the recorded values.
+        valid_loss = sum(valid_loss) / len(valid_loss)
+        valid_acc = sum(valid_accs) / len(valid_accs)
+
+        # Print the information.
+        print(f"[ Valid | {epoch + 1:03d}/{n_epochs:03d} ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f}")
+        
+        # if the model improves, save a checkpoint at this epoch
+        if valid_acc > best_acc:
+            best_acc = valid_acc
+            torch.save(model.state_dict(), model_path)
+            print('saving model with acc {:.3f}'.format(best_acc))
+
+def predict_data(test_loader, model_path, device):
+
+    # create model and load weights from checkpoint
+    model = Classifier().to(device)
+    model.load_state_dict(torch.load(model_path))
+
+    # Make sure the model is in eval mode.
+    # Some modules like Dropout or BatchNorm affect if the model is in training mode.
+    model.eval()
+
+    # Initialize a list to store the predictions.
+    predictions = []
+
+    # Iterate the testing set by batches.
+    for batch in tqdm(test_loader):
+        # A batch consists of image data and corresponding labels.
+        # But here the variable "labels" is useless since we do not have the ground-truth.
+        # If printing out the labels, you will find that it is always 0.
+        # This is because the wrapper (DatasetFolder) returns images and labels for each batch,
+        # so we have to create fake labels to make it work normally.
+        imgs, labels = batch
+
+        # We don't need gradient in testing, and we don't even have labels to compute loss.
+        # Using torch.no_grad() accelerates the forward process.
+        with torch.no_grad():
+            logits = model(imgs.to(device))
+
+        # Take the class with greatest logit as prediction and record it.
+        predictions.extend(logits.argmax(dim=-1).cpu().numpy().tolist())
+
+    # Save predictions into the file.
+    with open("predict.csv", "w") as f:
+
+        # The first row must be "Id, Category"
+        f.write("Id,Category\n")
+
+        # For the rest of the rows, each image id corresponds to a predicted class.
+        for i, pred in  enumerate(predictions):
+            f.write(f"{i},{pred}\n")
+
+def main():
+    print('------------------------------------------------------------------------------------------------------')
+    device = get_device()
+    print(f'DEVICE: {device}')
+    batch_size = config['batch_size']
+    learning_rate = config['learning_rate']
+    num_epoch = config['num_epoch']     
+    model_path = config['model_path']
+    weight_decay = config['weight_decay']
+
+    train_loader, valid_loader, test_loader, train_set, valid_set, unlabeled_set, test_set = prep_dataloader(batch_size)
+    print('New train begins')
+    train_data(train_loader, valid_loader, train_set, unlabeled_set, model_path, device, learning_rate, weight_decay, num_epoch, batch_size)
+    predict_data(test_loader, model_path, device)
+    print('All done')
+
+if __name__ == '__main__':
+    main()
