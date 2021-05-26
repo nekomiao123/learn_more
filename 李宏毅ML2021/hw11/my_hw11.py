5e1f60c75853bb5bbd6daf1a7da3bbcfad25d42a
Lkiku <447140443@qq.com>
2021-05-26 10:38:09 +0800

    new

---
+++ 李宏毅ML2021/hw11/my_hw11.py
@@ -0,0 +1,318 @@
+# -*- coding: utf-8 -*-
+
+import cv2
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Function
+ 
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+
+import pandas as pd
+import math
+
+# use this to record my loss
+import wandb
+
+# Data Process
+"""
+The data is suitible for `torchvision.ImageFolder`. You can create a dataset with `torchvision.ImageFolder`. 
+Details for image augmentation please refer to the comments in the following codes.
+"""
+
+torch.cuda.set_device(1)
+
+train_path = 'real_or_drawing/train_data'
+test_path = 'real_or_drawing/test_data'
+
+train_name = '2k_strong_newlamb'
+
+# hyperparameter
+default_config = dict(
+    train_batch=32,
+    test_batch=128,
+    num_epochs=2000,
+    learning_rate=1e-3,
+    lamb = 0.0,
+
+    early_stop=200,
+    warm_up_epochs=5
+)
+
+# Initialize 
+wandb.init(project='hw11', entity='nekokiku', config=default_config, name=train_name)
+config = wandb.config
+
+
+
+def data_process(train_batch, test_batch):
+
+    source_transform = transforms.Compose([
+        # Turn RGB to grayscale. (Bacause Canny do not support RGB images.)
+        transforms.Grayscale(),
+        # cv2 do not support skimage.Image, so we transform it to np.array, 
+        # and then adopt cv2.Canny algorithm.
+        transforms.Lambda(lambda x: cv2.Canny(np.array(x), 170, 300)),
+        # Transform np.array back to the skimage.Image.
+        transforms.ToPILImage(),
+        # 50% Horizontal Flip. (For Augmentation)
+        transforms.RandomHorizontalFlip(),
+        # Rotate +- 15 degrees. (For Augmentation), and filled with zero 
+        # if there's empty pixel after rotation.
+        transforms.RandomRotation(15, fill=(0,)),
+        # Transform to tensor for model inputs.
+        transforms.ToTensor(),
+    ])
+    target_transform = transforms.Compose([
+        # Turn RGB to grayscale.
+        transforms.Grayscale(),
+        # Resize: size of source data is 32x32, thus we need to 
+        #  enlarge the size of target data from 28x28 to 32x32。
+        transforms.Resize((32, 32)),
+        # 50% Horizontal Flip. (For Augmentation)
+        transforms.RandomHorizontalFlip(),
+        # Rotate +- 15 degrees. (For Augmentation), and filled with zero 
+        # if there's empty pixel after rotation.
+        transforms.RandomRotation(15, fill=(0,)),
+        # Transform to tensor for model inputs.
+        transforms.ToTensor(),
+    ])
+
+    source_dataset = ImageFolder(train_path, transform=source_transform)
+    target_dataset = ImageFolder(test_path, transform=target_transform)
+
+    source_dataloader = DataLoader(source_dataset, batch_size=train_batch, shuffle=True)
+    target_dataloader = DataLoader(target_dataset, batch_size=train_batch, shuffle=True)
+    test_dataloader = DataLoader(target_dataset, batch_size=test_batch, shuffle=False)
+
+    return source_dataset, target_dataset, source_dataloader, target_dataloader, test_dataloader
+
+"""# Model
+Feature Extractor: Classic VGG-like architecture
+Label Predictor / Domain Classifier: Linear models.
+"""
+
+class FeatureExtractor(nn.Module):
+
+    def __init__(self):
+        super(FeatureExtractor, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(128, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(256, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(256, 512, 3, 1, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+    def forward(self, x):
+        x = self.conv(x).squeeze()
+        return x
+
+class LabelPredictor(nn.Module):
+
+    def __init__(self):
+        super(LabelPredictor, self).__init__()
+
+        self.layer = nn.Sequential(
+            nn.Linear(512, 512),
+            nn.ReLU(),
+
+            nn.Linear(512, 512),
+            nn.ReLU(),
+
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, h):
+        c = self.layer(h)
+        return c
+
+class DomainClassifier(nn.Module):
+
+    def __init__(self):
+        super(DomainClassifier, self).__init__()
+
+        self.layer = nn.Sequential(
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+
+            nn.Linear(512, 1),
+        )
+
+    def forward(self, h):
+        y = self.layer(h)
+        return y
+
+def train(learning_rate, num_epochs,source_dataloader, target_dataloader, lamb):
+
+    '''
+      Args:
+        source_dataloader: source data的dataloader
+        target_dataloader: target data的dataloader
+        lamb: control the balance of domain adaptatoin and classification.
+    '''
+
+    feature_extractor = FeatureExtractor().cuda()
+    label_predictor = LabelPredictor().cuda()
+    domain_classifier = DomainClassifier().cuda()
+
+    class_criterion = nn.CrossEntropyLoss()
+    domain_criterion = nn.BCEWithLogitsLoss()
+
+    optimizer_F = optim.Adam(feature_extractor.parameters(), lr=learning_rate)
+    optimizer_C = optim.Adam(label_predictor.parameters(), lr=learning_rate)
+    optimizer_D = optim.Adam(domain_classifier.parameters(), lr=learning_rate)
+
+    # learning rate schedule
+    # warm_up_with_cosine_lr = lambda epoch: epoch / config['warm_up_epochs'] if epoch <= config['warm_up_epochs'] else 0.5 * ( math.cos((epoch - config['warm_up_epochs']) /(num_epochs - config['warm_up_epochs']) * math.pi) + 1)
+    # scheduler = torch.optim.lr_scheduler.LambdaLR( optimizer, lr_lambda=warm_up_with_cosine_lr)
+
+    for epoch in range(num_epochs):
+        # set the lambda
+        lamb = lamb + 1/num_epochs
+        lambd = 4. / (1. + math.exp(-10 * lamb)) - 2
+
+        # D loss: Domain Classifier的loss
+        # F loss: Feature Extrator & Label Predictor的loss
+        running_D_loss, running_F_loss = 0.0, 0.0
+        total_hit, total_num = 0.0, 0.0
+
+        for i, ((source_data, source_label), (target_data, _)) in enumerate(zip(source_dataloader, target_dataloader)):
+
+            source_data = source_data.cuda()
+            source_label = source_label.cuda()
+            target_data = target_data.cuda()
+
+            # Mixed the source data and target data, or it'll mislead the running params
+            #   of batch_norm. (runnning mean/var of soucre and target data are different.)
+            mixed_data = torch.cat([source_data, target_data], dim=0)
+            domain_label = torch.zeros([source_data.shape[0] + target_data.shape[0], 1]).cuda()
+            # set domain label of source data to be 1.
+            domain_label[:source_data.shape[0]] = 1
+
+            # Step 1 : train domain classifier
+            feature = feature_extractor(mixed_data)
+            # We don't need to train feature extractor in step 1.
+            # Thus we detach the feature neuron to avoid backpropgation.
+            domain_logits = domain_classifier(feature.detach())
+            loss = domain_criterion(domain_logits, domain_label)
+            running_D_loss+= loss.item()
+            loss.backward()
+            optimizer_D.step()
+
+            # Step 2 : train feature extractor and label classifier
+            class_logits = label_predictor(feature[:source_data.shape[0]])
+            domain_logits = domain_classifier(feature)
+            # loss = cross entropy of classification - lamb * domain binary cross entropy.
+            #  The reason why using subtraction is similar to generator loss in disciminator of GAN
+            loss = class_criterion(class_logits, source_label) - lambd * domain_criterion(domain_logits, domain_label)
+            running_F_loss+= loss.item()
+            loss.backward()
+            optimizer_F.step()
+            optimizer_C.step()
+
+            optimizer_D.zero_grad()
+            optimizer_F.zero_grad()
+            optimizer_C.zero_grad()
+
+            total_hit += torch.sum(torch.argmax(class_logits, dim=1) == source_label).item()
+            total_num += source_data.shape[0]
+            # print(i, end='\r')
+
+        train_D_loss, train_F_loss, train_acc = running_D_loss / (i+1), running_F_loss / (i+1), total_hit / total_num
+
+        torch.save(feature_extractor.state_dict(), 'model/extractor_{}.pt'.format(train_name))
+        torch.save(label_predictor.state_dict(), 'model/predictor_{}.pt'.format(train_name))
+        print('epoch {:>3d}: train D loss: {:6.4f}, train F loss: {:6.4f}, acc {:6.4f}'.format(epoch, train_D_loss, train_F_loss, train_acc))
+
+        # wandb
+        wandb.log({'epoch': epoch + 1, 'train_D_loss': train_D_loss, 'train_F_loss':train_F_loss, 'train_acc':train_acc, 'lambd':lambd})
+
+def predict(test_dataloader):
+
+    # load trained model
+    label_predictor_path = 'model/predictor_'+ train_name +'.pt'
+    feature_extractor_path = 'model/extractor_'+ train_name +'.pt'
+
+    label_predictor = LabelPredictor().cuda()
+    feature_extractor = FeatureExtractor().cuda()
+
+    label_predictor.load_state_dict(torch.load(label_predictor_path))
+    feature_extractor.load_state_dict(torch.load(feature_extractor_path))
+
+    label_predictor.eval()
+    feature_extractor.eval()
+
+    result = []
+    with torch.no_grad():
+        for i, (test_data, _) in enumerate(test_dataloader):
+            test_data = test_data.cuda()
+
+            feature = feature_extractor(test_data)
+
+            class_logits = label_predictor(feature)
+
+            x = torch.argmax(class_logits, dim=1).cpu().detach().numpy()
+            result.append(x)
+
+    result = np.concatenate(result)
+
+    # Generate your submission
+    df = pd.DataFrame({'id': np.arange(0,len(result)), 'label': result})
+    df.to_csv('result/' + train_name + '_submission.csv',index=False)
+
+def main():
+    train_batch = config['train_batch']
+    test_batch = config['test_batch']
+    lamb = config['lamb']
+    num_epochs = config['num_epochs']
+    learning_rate = config['learning_rate']
+
+    print("loading data")
+    source_dataset, target_dataset, source_dataloader, target_dataloader, test_dataloader = data_process(train_batch, test_batch)
+    print("begin trainning")
+    train(learning_rate, num_epochs,source_dataloader, target_dataloader, lamb)
+    print("begin testing")
+    predict(test_dataloader)
+    print("Done!!!!!!")
+
+
+if __name__ == '__main__':
+    main()
